cc829974c12b8badd215b7e3df31bd91a0dec928
mjh5 <mjh5@illinois.edu>
2018-06-12 17:42:59 -0500

    Examples filled out

--- java/src/WorldCup2014Example.java
+++ java/src/WorldCup2014Example.java
@@ -3,6 +3,19 @@ public class WorldCup2014Example {
     public static void main(String[] args) {
         WorldCup worldCup = JSONParser.parseDataFromFilePath("C:\\Users\\micha\\SummerProjects2018\\WorldCupJSONParser\\res\\worldcup2014json");
 
+        //Will print "World Cup 2014"
         System.out.println(worldCup.getName());
+
+        //An array of all the rounds in the world cup
+        Round[] rounds = worldCup.getRounds();
+
+        //Will print "BRA"
+        System.out.println(rounds[0].getMatches()[0].getTeam1().getCode());
+
+        //The final match of the 2014 World Cup
+        Match championship = rounds[rounds.length - 1].getMatches()[0];
+
+        //Will print " Mario Gotze"
+        System.out.println(championship.getGoals()[0].getName());
     }
 }
