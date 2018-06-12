03aa3d102dce2de8287f11cf7962f481e015ae6c
mjh5 <mjh5@illinois.edu>
2018-06-12 13:25:40 -0500

    Added data fields to Match class

--- java/src/Match.java
+++ java/src/Match.java
@@ -1,2 +1,15 @@
 public class Match {
+    //Data fields parsed from JSON
+    int num;
+    String date;
+    String time;
+    Team team1;
+    Team team2;
+    int score1;
+    int score2;
+    Goal[] goals;
+    String group;
+    Stadium stadium;
+    String city;
+    String timezone;
 }
