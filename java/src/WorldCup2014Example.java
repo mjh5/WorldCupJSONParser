70dfcba536387ed6a06781c55aea77fe8166a443
mjh5 <mjh5@illinois.edu>
2018-06-12 17:30:34 -0500

    Wrote main method in WorldCup2014Example

--- java/src/WorldCup2014Example.java
+++ java/src/WorldCup2014Example.java
@@ -1,2 +1,8 @@
 public class WorldCup2014Example {
+
+    public static void main(String[] args) {
+        WorldCup worldCup = JSONParser.parseLayout("../../res/worldcup2014.json");
+
+        System.out.println(worldCup.getName());
+    }
 }
